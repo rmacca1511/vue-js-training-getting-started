4263514212f1e6846c5acb95633a0e6d39d66cd8
Ryan Mackenzie <ryan.mackenzie@bekagool.com>
2019-07-09 11:10:25 +0100

    Binding CSS properties and Logo

--- js/app.js
+++ js/app.js
@@ -2,7 +2,13 @@ $(document).ready(function () {
     var growler = new Vue({
         el: '#growler',
         data: {
-            appName: '<a href="./">Growler</a>'
+            appName: '<a href="./">Growler</a>',
+            appLogo: '../img/logo.png',
+            appStyle: {
+                color: '#FF6A00',
+                fontFamily:'Verdana',
+                margin: 0    
+            }
         }
     });
 });
